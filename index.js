eb554cd77d8d19c067ce30cc633ad58f341880bd
serkansahin55 <ssahin55@outlook.com>
2020-12-11 13:52:50 -0500

    commit tree 04e3541c34a5a38d8a2acb861e20e7209f380408

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Serkan is the champion!"
+document.querySelector("main#main").remove();
+
